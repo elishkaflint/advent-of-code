022479347ab455b3d839b475d0fe8505ee051c06
Elishka Flint <elishkaf@gmail.com>
2018-12-01 23:38:48 +0000

    Rearrange file structure

--- src/main/java/DayOne/HttpExample.java
+++ src/main/java/DayOne/HttpExample.java
@@ -1,11 +1,9 @@
+package DayOne;
+
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class HttpExample {
 
--- src/test/java/DayOne/FrequencyTest.java
+++ src/test/java/DayOne/FrequencyTest.java
@@ -1,3 +1,5 @@
+package DayOne;
+
 import org.junit.jupiter.api.Test;
 import java.util.logging.Logger;
 
