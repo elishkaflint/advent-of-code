5d59cf0404042925574d35e28614e68efc9fec62
Elishka Flint <elishkaf@gmail.com>
2018-12-03 22:26:43 +0000

    Add calc for max coordinates

--- src/main/java/DayThree/Fabric.java
+++ src/main/java/DayThree/Fabric.java
@@ -13,12 +13,38 @@ public class Fabric {
         this.claims = (ArrayList<String>) new FileConverter().csvToArrayList(file);
     }
 
+    public int getMaxX() {
+        int maxX = 0;
+        for (String line : this.claims) {
+            Claim claim = new Claim(line);
+            int x = claim.getX() + claim.getWidth();
+            if( x > maxX ){ maxX = x; }
+        }
+        return maxX;
+    }
+
+    public int getMaxY() {
+        int maxY = 0;
+        for (String line : this.claims) {
+            Claim claim = new Claim(line);
+            int y = claim.getY() + claim.getHeight();
+            if( y > maxY ){ maxY = y; }
+        }
+        return maxY;
+    }
+
     public int calculate() {
 
         List<ArrayList<Integer>> map = new ArrayList<>();
 
         return 1;
 
+    }
+
+    public static void main (String[] args) throws Exception {
+        Fabric fabric = new Fabric("claims-inputs.csv");
+        System.out.println(fabric.getMaxX());
+        System.out.println(fabric.getMaxY());
     }
 
 }
