7314acec4b29d7cd8c6b506f8d6d677325c0cf6c
Elishka Flint <elishkaf@gmail.com>
2018-12-03 22:15:22 +0000

    Create claim class to parse input data

--- src/main/java/DayThree/Claim.java
+++ src/main/java/DayThree/Claim.java
@@ -9,17 +9,32 @@ public class Claim {
     }
 
     public int getX() {
-        return 1;
+        return Integer.valueOf(parse(this.claim)[4]);
     }
 
-    public String[] parse() {
-        String[] parsed = this.claim.split("[0-9]+|[A-Z]+");
+    public int getY() {
+        return Integer.valueOf(parse(this.claim)[5]);
+    }
+
+    public int getWidth() {
+        return Integer.valueOf(parse(this.claim)[7]);
+    }
+
+    public int getHeight() {
+        return Integer.valueOf(parse(this.claim)[8]);
+    }
+
+    public static String[] parse(String claim) {
+        String[] parsed = claim.split("[^0-9]");
         return parsed;
     }
 
     public static void main (String[] args) throws Exception {
         Claim claim = new Claim("#1 @ 146,196: 19x14");
-        System.out.println(claim.parse().length);
+        System.out.println(claim.getX());
+        System.out.println(claim.getY());
+        System.out.println(claim.getHeight());
+        System.out.println(claim.getWidth());
     }
 
 }
