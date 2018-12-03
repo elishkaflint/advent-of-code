a738b3d745f7c64e49a6e7cd1f9d062a3f992518
Elishka Flint <elishkaf@gmail.com>
2018-12-03 23:00:23 +0000

    Add generate matrix function which is taking forever to run

--- src/main/java/DayThree/Fabric.java
+++ src/main/java/DayThree/Fabric.java
@@ -33,18 +33,36 @@ public class Fabric {
         return maxY;
     }
 
+    public List<List<Integer>> generateMatrix() {
+        List<List<Integer>> rows = new ArrayList<>();
+        for(int i = 0; i < getMaxY(); i++) {
+            List<Integer> columns = new ArrayList<>();
+            for(int j = 0; j < getMaxX(); j++) {
+                columns.add(j, 0);
+            }
+            rows.add(i, columns);
+        }
+        return rows;
+    }
+
+    public void populateMatrix() {
+
+    }
+
     public int calculate() {
+        // count frequency of any number >= 2
+
+
 
         List<ArrayList<Integer>> map = new ArrayList<>();
-
         return 1;
-
     }
 
     public static void main (String[] args) throws Exception {
         Fabric fabric = new Fabric("claims-inputs.csv");
         System.out.println(fabric.getMaxX());
         System.out.println(fabric.getMaxY());
+        System.out.println(fabric.generateMatrix());
     }
 
 }
